35eba6a2789851e243767e0bb03dc71ceaa6da43
jnelson <61723804+jnel-221@users.noreply.github.com>
2021-04-11 19:39:33 -0500

    server built and connecting, created models folder

---
+++ server.js
@@ -0,0 +1,40 @@
+//require dependencies
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.port || 3000;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+//set up express middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workouttrackerdb",
+  { useNewUrlParser: true }
+);
+
+// HTML routes
+
+//Route to get index.html
+
+//route to get exercise.html
+
+//API routes
+
+//route to post form submission to mongoDB via mongoose
+
+//route to get stats.html populated w/data
+
+//start server
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
